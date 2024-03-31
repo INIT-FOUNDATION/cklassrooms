855f18b77e0bd0bc43128ea1ad404ef847a62f92
AkatsukiShubham <shubhambaranwal2580@gmail.com>
2024-03-31 17:54:28 +0530

    Integrated animations

--- cklassroom-frontend-homepage/src/app/app.component.ts
+++ cklassroom-frontend-homepage/src/app/app.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-
+import * as AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, AfterViewInit {  
-  display = false;  
-  constructor(
+export class AppComponent implements OnInit, AfterViewInit {
+  display = false;
+  constructor() {}
 
-  ) { }
-
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    AOS.init();
+  }
 
   ngAfterViewInit(): void {}
 
-  ngOnDestroy(): void {
-  }
+  ngOnDestroy(): void {}
 }
