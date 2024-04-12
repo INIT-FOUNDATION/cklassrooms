92ec1a39f0fa984e840597a0f37d8c07f08ce2e4
AkatsukiShubham <shubhambaranwal2580@gmail.com>
2024-04-12 21:17:14 +0530

    Working on UI designs

--- cklassroom-frontend-homepage/src/app/modules/shared/components/apply-dialog/apply-dialog.component.ts
+++ cklassroom-frontend-homepage/src/app/modules/shared/components/apply-dialog/apply-dialog.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-apply-dialog',
   templateUrl: './apply-dialog.component.html',
-  styleUrls: ['./apply-dialog.component.scss']
+  styleUrls: ['./apply-dialog.component.scss'],
 })
 export class ApplyDialogComponent implements OnInit {
-  activeScreen = "step1";
+  activeScreen = 'step1';
   step1Form: FormGroup;
   step2Form: FormGroup;
-  constructor() { }
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.initForms();
   }
-
 
   initForms() {
     this.step1Form = new FormGroup({
@@ -35,27 +35,27 @@ export class ApplyDialogComponent implements OnInit {
   }
 
   onDropDownChange(formControlName: string) {
-    const value =  this.step2Form.get(formControlName).value;
+    const value = this.step2Form.get(formControlName).value;
     if (formControlName === 'working') {
-      if (value === "yes") {
-        this.step2Form.get("job_domain").setValidators([Validators.required]);
-        this.step2Form.get("job_role").setValidators([Validators.required]);
-        this.step2Form.get("company").setValidators([Validators.required]);
+      if (value === 'yes') {
+        this.step2Form.get('job_domain').setValidators([Validators.required]);
+        this.step2Form.get('job_role').setValidators([Validators.required]);
+        this.step2Form.get('company').setValidators([Validators.required]);
       } else {
-        this.step2Form.get("job_domain").clearValidators();
-        this.step2Form.get("job_role").clearValidators();
-        this.step2Form.get("company").clearValidators();
+        this.step2Form.get('job_domain').clearValidators();
+        this.step2Form.get('job_role').clearValidators();
+        this.step2Form.get('company').clearValidators();
       }
-      this.step2Form.get("job_domain").updateValueAndValidity();
-      this.step2Form.get("job_role").updateValueAndValidity();
-      this.step2Form.get("company").updateValueAndValidity();
+      this.step2Form.get('job_domain').updateValueAndValidity();
+      this.step2Form.get('job_role').updateValueAndValidity();
+      this.step2Form.get('company').updateValueAndValidity();
     }
   }
 
   changeScreen(name) {
-    if (name === "step2") {
+    if (name === 'step2') {
       const step1FormValid = this.step1Form.valid;
-      if(step1FormValid) {
+      if (step1FormValid) {
         this.activeScreen = name;
       }
     } else {
@@ -63,4 +63,7 @@ export class ApplyDialogComponent implements OnInit {
     }
   }
 
+  closeModal() {
+    this.dialog.closeAll();
+  }
 }
--- cklassroom-frontend-homepage/src/app/modules/shared/theme/themes.const.ts
+++ cklassroom-frontend-homepage/src/app/modules/shared/theme/themes.const.ts
@@ -1,16 +1,16 @@
 export const themes = {
   light_theme: {
-    '--themePrimary': '#F9F7F7',
+    '--themePrimary': '#F9F7F8',
     '--themeSecondary': '#11b685',
     '--themeFontColor': '#112D4E',
     '--themeGray': '#ffffff',
-    '--themeBoxShadow': '#eee',
+    '--themeBoxShadow': '#e0e0e0',
     '--themeHoverBoxShadow': '#e3dddd',
     '--themeDialogModal': '#112D4E',
   },
   dark_theme: {
     '--themePrimary': '#1b1f24',
-    '--themeSecondary': '#16daaa',
+    '--themeSecondary': '#11b685',
     '--themeFontColor': '#ffffff',
     '--themeGray': '#3f3d56',
     '--themeBoxShadow': '#27272a',
