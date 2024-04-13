41f2961ad5b23acd9200ea21b8a414500b204cd6
Narsima Chilkuri <nchilkur@cisco.com>
2024-04-13 14:13:32 +0530

    mail notifications added for enquiry

--- cklassroom-micro-crm/services/crmService.ts
+++ cklassroom-micro-crm/services/crmService.ts
@@ -1,4 +1,4 @@
-import { mongoDB, logger } from "cklasroom-micro-common";
+import { mongoDB, logger, nodemailerUtils, ejsUtils } from "cklasroom-micro-common";
 import { EnquiryUser } from "../model/enquiryUser";
 import { MONGO_COLLECTIONS } from "../constants/MONGO_COLLECTIONS";
 
@@ -7,10 +7,30 @@ const enrollStudent = async (enquiryObj: EnquiryUser) => {
   try {
     logger.info(`crmService :: enrollStudent :: enquiryObj :: ${JSON.stringify(enquiryObj)}`);
     const data = await mongoDB.insertOne(MONGO_COLLECTIONS.CRM_ENQUIRY_USERS, enquiryObj);
+    sendEnquiryNotification(enquiryObj);
     logger.info(`crmService :: enrollStudent :: enquiryObj :: ${JSON.stringify(enquiryObj)} :: after saving :: ${JSON.stringify(data)}`);
     return data;
   } catch (error) {
     logger.error(error);
+    throw error;
+  }
+}
+
+const sendEnquiryNotification = async (enquiryObj: EnquiryUser) => {
+  try {
+    const ccEmails = process.env.CKLASSROOM_CRM_CC_EMAILS.split(",")
+    
+    const enquiryRequestHtml = await ejsUtils.generateHtml("views/enquiryRequest.ejs", enquiryObj)
+    const enquiryResponseHtml = await ejsUtils.generateHtml("views/enquiryResponse.ejs", {
+      name: enquiryObj.name
+    })
+
+    await nodemailerUtils.sendEmail("CKLASSROOMS | Enquiry Request", enquiryRequestHtml, null, ccEmails, [])
+    await nodemailerUtils.sendEmail("CKLASSROOMS | Enquiry Response", enquiryResponseHtml, enquiryObj.email, ccEmails, [])
+
+    logger.info(`crmService :: sendEnquiryNotification :: Notifications Sent Successfully`);
+  } catch (error) {
+    logger.error(`crmService :: sendEnquiryNotification :: ${error}`)
     throw error;
   }
 }
