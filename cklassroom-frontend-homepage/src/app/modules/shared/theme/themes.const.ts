81260cbcd64983e4910164c10207e3c5ecbabead
shubham-orrizonte <shubham.baranwal@orrizonte.in>
2024-07-01 11:00:05 +0530

    Changed theme

--- cklassroom-frontend-homepage/src/app/modules/shared/theme/themes.const.ts
+++ cklassroom-frontend-homepage/src/app/modules/shared/theme/themes.const.ts
@@ -1,6 +1,7 @@
 export const themes = {
   light_theme: {
     '--themePrimary': '#F9F7F8',
+    '--themePrimary-2': '#F2F2F2',
     '--themeSecondary': '#11b685',
     '--themeFontColor': '#112D4E',
     '--themeGray': '#ffffff',
@@ -11,11 +12,12 @@ export const themes = {
   },
   dark_theme: {
     '--themePrimary': '#1b1f24',
+    '--themePrimary-2': '#292d33',
     '--themeSecondary': '#11b685',
     '--themeFontColor': '#ffffff',
     '--themeGray': '#3f3d56',
     '--themeBoxShadow': '#73798129',
-    '--themeHoverBoxShadow': '#1b1f24',
+    '--themeHoverBoxShadow': 'transparent',
     '--themeDialogModal': '#112D4E',
     '--themeButtonColor': '#4167f0',
   },
